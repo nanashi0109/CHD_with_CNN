10e3963c051fcb2ee98d91e74b7cb0c94d093dfb
nanashi <egorglazkov71@gmail.com>
2025-05-12 19:06:50 +0300

    feat(main): add app using FastApi

---
+++ src/main.py
@@ -0,0 +1,21 @@
+from ImageData import ImageModel
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", reload=True)
